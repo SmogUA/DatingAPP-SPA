9f07bc05dc4590d2182f4faafeccf360064c9c69
Smog <smog@itgalaxy.net>
2019-12-11 09:50:19 -0800

    57

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -9,7 +9,7 @@ import {AlertifyService} from '../_services/alertify.service';
 })
 export class NavComponent implements OnInit {
 model: any = {};
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
@@ -22,9 +22,9 @@ model: any = {};
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+        return this.authService.loggedIn();
   }
+
   logout() {
 localStorage.removeItem('token');
 this.alertify.message('Logged Out');
