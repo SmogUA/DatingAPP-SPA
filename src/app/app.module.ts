035e9cb9cf7da61bf0d1a9278a5f7cbe3bdbd35d
Smog <smog@itgalaxy.net>
2020-01-17 05:32:28 -0800

    110

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,6 +27,7 @@ import {NgxGalleryModule} from 'ngx-gallery';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import {MemberEditResolver} from './_resolvers/member-edit.resolver';
 import {PreventUnsavedChanges} from './_guards/prevent-unsaved-changes.guard';
+import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 
 export function tokenGetter() {
 return localStorage.getItem('token');
@@ -50,7 +51,8 @@ export class CustomHammerConfig extends HammerGestureConfig  {
     MessagesComponent,
     MemberCardComponent,
     MemberDetailComponent,
-    MemberEditComponent
+    MemberEditComponent,
+    PhotoEditorComponent
   ],
   imports: [
     BrowserModule,
